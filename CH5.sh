f88d2b111dcf62c87d7330503e31b85777f9666c
rarar0/0 <boombot101@gmail.com>
2018-02-13 21:53:17 -0600

    Create CH5.sh

---
+++ CH5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#CSCI 2461-70\
+#Alex Malm
+
+# Change root file
+sudo root = /dev/sdc1
+
+#Change it again!
+root = /dev/sdc2
+
+#View kernel parameters
+cat /proc/cmdline
+
+echo "Root has been moved to /dev/sdc2
